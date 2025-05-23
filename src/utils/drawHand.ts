8419010d954198e86764c591ed4b3d00a1f63d9d
Mrunu2122 <mrunalsoshte212208@gmail.com>
2025-05-23 15:05:29 +0530

    change for deployment

--- src/utils/drawHand.ts
+++ src/utils/drawHand.ts
@@ -1,10 +1,18 @@
-export const drawHand = (predictions: any, ctx: CanvasRenderingContext2D) => {
+type Prediction = {
+  landmarks: [number, number, number][];
+};
+
+export const drawHand = (
+  predictions: Prediction[],
+  ctx: CanvasRenderingContext2D
+) => {
   if (!predictions.length) return;
 
-  predictions.forEach((prediction: any) => {
+  predictions.forEach((prediction) => {
     const landmarks = prediction.landmarks;
     ctx.fillStyle = "red";
-    landmarks.forEach(([x, y]: number[]) => {
+
+    landmarks.forEach(([x, y]) => {
       ctx.beginPath();
       ctx.arc(x, y, 5, 0, 2 * Math.PI);
       ctx.fill();
